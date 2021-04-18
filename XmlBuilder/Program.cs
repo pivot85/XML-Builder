614dd99f63f0e5c8db3ca05a6081bde02803f8d9
Saud <saud.alshammari@tuwaiq.edu.sa>
2021-04-19 01:52:28 +0300

    add : xml builder

---
+++ XmlBuilder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace XmlBuilder
+{
+    class Program
+    {
+
+        class XmlBuilder {
+            private string xml;
+
+            public XmlBuilder()
+            {
+                this.xml = "";
+            }
+
+            public XmlBuilder beginTag(string value)
+            {
+                this.xml += "<" + value + ">";
+                return this;
+            }
+            public XmlBuilder content(string value)
+            {
+                this.xml += " " + value + " ";
+                return this;
+            }
+            public XmlBuilder endTag(string value)
+            {
+                this.xml += "</" + value + ">";
+                return this;
+            }
+
+            public string get()
+            {
+                return this.xml;
+            }
+        }
+        static void Main(string[] args)
+        {
+            XmlBuilder x = new XmlBuilder();
+
+            x.beginTag("note")
+             .beginTag("to")
+             .content("Mohammed")
+             .endTag("to")
+
+             .beginTag("from")
+             .content("SAUD")
+             .endTag("from")
+
+             .beginTag("heading")
+             .content("Remainder")
+             .endTag("heading")
+
+             .beginTag("body")
+             .content("Submit the assignment on time")
+             .endTag("body")
+
+              .endTag("note");
+
+            Console.WriteLine(x.get());
+
+        }
+    }
+}
